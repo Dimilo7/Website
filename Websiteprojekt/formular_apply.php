91c200d3060592651af25ac67359f483c1cf46b2
Marco Schuppisser <marcoschuppi@hotmail.ch>
2020-03-15 15:05:47 +0100

    commit #63 php comments

--- Websiteprojekt/formular_apply.php
+++ Websiteprojekt/formular_apply.php
@@ -1,31 +1,31 @@
 <?php
 
 try {
-  $user = 'caught_123admin'; #unser benutzername und passwort, sollte nach eigennutzen angepasst werden
+  $user = 'caught_123admin'; #our username and password, which should be adapted
   $pass = '-q5BysB.Z5YtGDj';
-  $inputArray = [ 'surName' => $_POST['surName'],
+  $inputArray = [ 'surName' => $_POST['surName'], //array with all variables which will be put into the db
                   'lastName' => $_POST['lastName'],
                   'eMail' => $_POST['eMail'],
                   'phoneNumber' => $_POST['phoneNumber'],
                   'motivation' => $_POST['motivation'],
                   'comment' => $_POST['comment'],
                 ];
-    $tableName = 'apply';
+    $tableName = 'apply'; //name of the table in the variable $tablename
 
-    $pdo = new PDO('mysql:host=localhost;dbname=applications', $user, $pass);
+    $pdo = new PDO('mysql:host=localhost;dbname=applications', $user, $pass); //creating connection with the db
 
-    $sql = "INSERT INTO " .$tableName." (surName, lastName, eMail, phoneNumber, motivation, comment) /*SQL Code in String*/
-            VALUES (:surName, :lastName, :eMail, :phoneNumber, :motivation, :comment)";
+    $sql = "INSERT INTO " .$tableName." (surName, lastName, eMail, phoneNumber, motivation, comment)
+            VALUES (:surName, :lastName, :eMail, :phoneNumber, :motivation, :comment)"; //this sql code determines what values go in which table list
 
     $stmt= $pdo->prepare($sql);
-    $stmt->execute($inputArray);
+    $stmt->execute($inputArray); //inserting the values of the array into the table
 
     $dbh = null;
-} catch (PDOException $exception) { /*wenn irgendwo error, print error and die = get me out of here, $exception = error*/
+} catch (PDOException $exception) { //if the connection fails, the program "catches" the user here and displays an error message
     print "Error!: " . $exception->getMessage() . "<br/>";
     die();
 }
 
-require_once __DIR__ . "/apply.html";
+require_once __DIR__ . "/apply.html"; //directory of the html file
 
 ?>
--- Websiteprojekt/formular_booking.php
+++ Websiteprojekt/formular_booking.php
@@ -1,45 +1,45 @@
 <?php
 
 try {
-  $user = 'caught_123admin'; #unser benutzername und passwort, sollte nach eigennutzen angepasst werden
+  $user = 'caught_123admin'; #our username and password, which should be adapted
   $pass = '-q5BysB.Z5YtGDj';
-  $inputArray = [ 'surName' => $_POST['surName'],
+  $inputArray = [ 'surName' => $_POST['surName'], //array with all variables which will be put into the db
                   'lastName' => $_POST['lastName'],
                   'eMail' => $_POST['eMail'],
                   'phoneNumber' => $_POST['phoneNumber'],
                   'comment' => $_POST['comment'],
                 ];
-  $tableName = '';
+  $tableName = ''; //initialize variable $tablename
 
 
-    switch($_POST['type']) {
-      case 'photoshooting':
+    switch($_POST['type']) { //switch is like multiple if statements, it chooses which form was sent to the server and adjusts the tablename based off of it
+      case 'photoshooting': //photoshooting form
       $tableName = 'photoshooting';
       break;
 
-    case 'coaching':
-      $tableName = 'coaching';
+    case 'coaching': //coaching form
+      $tableName = 'coaching' ;
       break;
 
-    case 'course':
+    case 'course': //cuorse form
       $tableName = 'course';
       break;
     }
 
-    $pdo = new PDO('mysql:host=localhost;dbname=bookings', $user, $pass);
+    $pdo = new PDO('mysql:host=localhost;dbname=bookings', $user, $pass); //creating connection with db
 
     $sql = "INSERT INTO " .$tableName." (surName, lastName, eMail, phoneNumber, comment) /*SQL Code in String*/
-            VALUES (:surName, :lastName, :eMail, :phoneNumber, :comment)";
+            VALUES (:surName, :lastName, :eMail, :phoneNumber, :comment)"; //this sql code determines what values go in which table list
 
     $stmt= $pdo->prepare($sql);
-    $stmt->execute($inputArray);
+    $stmt->execute($inputArray); //inserting the values of the array into the table
 
     $dbh = null;
-} catch (PDOException $exception) { /*wenn irgendwo error, print error and die = get me out of here, $exception = error*/
+} catch (PDOException $exception) { //if the connection fails, the program "catches" the user here and displays an error message
     print "Error!: " . $exception->getMessage() . "<br/>";
     die();
 }
 
-require_once __DIR__ . "/book-us.html";
+require_once __DIR__ . "/book-us.html"; //directory of the html file
 
 ?>
